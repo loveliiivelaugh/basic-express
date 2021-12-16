d9180661f684ecc60c3c226b4dd626a894b62384
Michael Woodward <44563439+MichaelWoodward90@users.noreply.github.com>
2021-12-16 07:56:55 -0600

    Added a root route for API.

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 
 module.exports = router;
